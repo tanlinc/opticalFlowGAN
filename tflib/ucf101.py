cbca305c6cf263c70be87d08d62d9909d8bfdc55
Tanja Linkermann <tanja.linkermann@gmx.de>
2018-07-03 10:41:39 +0200

    data dir and code for using data, remove VAE as long as not working

--- tflib/ucf101.py
+++ tflib/ucf101.py
@@ -4,12 +4,115 @@ import os
 import urllib
 import gzip
 import cPickle as pickle
+from keras.preprocessing.image import ImageDataGenerator
+from UCFdata import DataSet
+
+data = DataSet()
 
 def unpickle(file):
     fo = open(file, 'rb')
     dict = pickle.load(fo)
     fo.close()
     return dict['data'], dict['labels']
+
+"""
+Train on images split into directories. This assumes we've split
+our videos into frames and moved them to their respective folders.
+Use keras 2+ and tensorflow 1+
+Based on:
+https://keras.io/preprocessing/image/
+"""
+def get_generators():
+    train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        horizontal_flip=True,
+        rotation_range=10.,
+        width_shift_range=0.2,
+        height_shift_range=0.2)
+
+    test_datagen = ImageDataGenerator(rescale=1./255)
+
+    train_generator = train_datagen.flow_from_directory(
+        './data/train/',
+        target_size=(299, 299),
+        batch_size=32,
+        classes=data.classes,
+        class_mode='categorical')
+
+    validation_generator = test_datagen.flow_from_directory(
+        './data/test/',
+        target_size=(299, 299),
+        batch_size=32,
+        classes=data.classes,
+        class_mode='categorical')
+
+    return train_generator, validation_generator
+# generators = get_generators()
+# def train_model(model, nb_epoch, generators, callbacks=[]):
+#    train_generator, validation_generator = generators
+ #   model.fit_generator(
+  #      train_generator,
+   #     steps_per_epoch=100,
+    #    validation_data=validation_generator,
+     #   validation_steps=10,
+      #  epochs=nb_epoch,
+       # callbacks=callbacks)
+    #return model
+
+
+  #  class_limit = None  # int, can be 1-101 or None
+  #  seq_length = 40
+  #  load_to_memory = False  # pre-load the sequences into memory
+  #  batch_size = 32
+  #  nb_epoch = 1000
+  #  image_shape = (80, 80, 3)
+
+def generator(train, batch_size=32, seq_length = 40, class_limit=None, image_shape=None,
+          load_to_memory=True):
+    # Get the data and process it.
+    if image_shape is None:
+        data = DataSet(
+            seq_length=seq_length,
+            class_limit=class_limit
+        )
+    else:
+        data = DataSet(
+            seq_length=seq_length,
+            class_limit=class_limit,
+            image_shape=image_shape
+        )
+
+    # Get samples per epoch.
+    # Multiply by 0.7 to attempt to guess how much of data.data is the train set.
+    steps_per_epoch = (len(data.data) * 0.7) // batch_size
+
+    #if load_to_memory:
+        # Get data.
+    X, y = data.get_all_sequences_in_memory('train', 'images')
+    X_test, y_test = data.get_all_sequences_in_memory('test', 'images')
+    #else:
+        # Get generators.
+       # generator = data.frame_generator(batch_size, 'train', 'images')
+        #val_generator = data.frame_generator(batch_size, 'test', 'images')
+
+    def get_epoch(data_type):
+        if(train):
+            rng_state = np.random.get_state()
+            np.random.shuffle(X)
+            np.random.set_state(rng_state)
+            np.random.shuffle(y)
+            for i in xrange(len(images) / batch_size):
+                yield (X[i*batch_size:(i+1)*batch_size], y[i*batch_size:(i+1)*batch_size])
+        else:
+            rng_state = np.random.get_state()
+            np.random.shuffle(X_test)
+            np.random.set_state(rng_state)
+            np.random.shuffle(y_test)
+            for i in xrange(len(images) / batch_size):
+                yield (X_test[i*batch_size:(i+1)*batch_size], y_test[i*batch_size:(i+1)*batch_size])
+
+    return get_epoch(train)
 
 def ucf101_generator(filenames, batch_size, data_dir):
     all_data = []
@@ -34,8 +137,8 @@ def ucf101_generator(filenames, batch_size, data_dir):
     return get_epoch
 
 
-def load(batch_size, data_dir):
+def load(batch_size):
     return (
-        ucf101_generator(['data_batch_1','data_batch_2','data_batch_3','data_batch_4','data_batch_5'], batch_size, data_dir), 
-        ucf101_generator(['test_batch'], batch_size, data_dir)
+        generator(True, batch_size), 
+        generator(False, batch_size)
     )
