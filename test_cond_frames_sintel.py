d377a2c6c19ac3aa88f6809aa255b183c905c3af
Tanja Linkermann <tanja.linkermann@gmx.de>
2018-11-07 14:12:27 +0100

    another try

--- test_cond_frames_sintel.py
+++ test_cond_frames_sintel.py
@@ -7,49 +7,43 @@ import tflib.save_images
 import tflib.flow_handler as fh
 import tflib.SINTELdata as sintel
 
-BATCH_SIZE = 64 # Batch size
+outpath = "/home/linkermann/opticalFlow/opticalFlowGAN/data/gentest/"
+
+BATCH_SIZE = 64 
 IM_DIM = 32 # number of pixels along x and y (square assumed)
 OUTPUT_DIM = IM_DIM*IM_DIM*3 # Number of pixels (3*32*32) - rgb color
-OUTPUT_DIM_FLOW = IM_DIM*IM_DIM*2 # Number of pixels (2*32*32) - uv direction
+OUTPUT_DIM_FLOW = IM_DIM*IM_DIM*2 # Number of pixels (2*32*32) - uv
 lib.print_model_settings(locals().copy())
 
-# Dataset iterators
-gen = sintel.load_train_gen(BATCH_SIZE, (IM_DIM,IM_DIM,3), (IM_DIM,IM_DIM,2)) # batch size, im size, im size flow
+# Dataset iterators: # batch size, im size, im size flow
+gen = sintel.load_train_gen(BATCH_SIZE, (IM_DIM,IM_DIM,3), (IM_DIM,IM_DIM,2)) 
+_data, _flow  = next(gen) # fixed_cond_samples, fixed_flow_samples # (batchsize, 3072) 
+fixed_cond_data_int = _data[:,0:2*OUTPUT_DIM] # earlier frames as cond, _data: (64,3*3072)
+fixed_viz_data_int = _data[:,OUTPUT_DIM:2*OUTPUT_DIM] # each later frame for viz
+fixed_real_data =_flow[:,OUTPUT_DIM_FLOW:] # later flow for discr, _flow: (64,2*2048)
 
-# For generating samples: define conditional input
-_data, _flow  = next(gen) # fixed_cond_samples, fixed_flow_samples = next(gen)  # shape: (batchsize, 3072) 
-fixed_cond_data_int = _data[:,0:2*OUTPUT_DIM] # earlier frames as condition, cond samples shape (64,3*3072)
-fixed_viz_data_int = _data[:,OUTPUT_DIM:2*OUTPUT_DIM]
+# images = fixed_viz_data_int.reshape(BATCH_SIZE,IM_DIM,IM_DIM,3)
+images = fixed_viz_data_int.reshape(BATCH_SIZE,3, IM_DIM,IM_DIM)
+tflib.save_images.save_images(images, outpath+"condvizbatch.jpg") # viz cond data
 
-# viz cond data
-images = fixed_viz_data_int.reshape(BATCH_SIZE, IM_DIM,IM_DIM,3)
-outpath = "/home/linkermann/opticalFlow/opticalFlowGAN/data/gentest/"
-tflib.save_images.save_images(images.reshape((BATCH_SIZE,3,IM_DIM,IM_DIM)), outpath+"condvizbatch.jpg")
-
-fixed_real_data =_flow[:,OUTPUT_DIM_FLOW:]	 # later flow for discr, flow samples shape (64,2048)
-
-init_op = tf.global_variables_initializer()  	# op to initialize the variables.
-
-# Train loop
 with tf.Session() as session:
-    session.run(init_op)
+    session.run(tf.global_variables_initializer())
 
-    # samples_255 = np.zeros((2*BATCH_SIZE, IM_DIM, IM_DIM, 3))
-    real_flowimages = []
     for i in range(0, BATCH_SIZE):
-        real_flowimg = [] # reset to be sure
-        real_flowimg = fh.computeFlowImg(fixed_real_data[i,:].reshape((IM_DIM,IM_DIM,2)))  # (32, 32, 3) # now color img!! :)
-        # real_flowimage_T = np.transpose(real_flowimg, [2,0,1])  #  (3, 32, 32)
-        # real_flowimages.append(real_flowimage) # np.asarray...      
-        real_flow = real_flowimg.astype('int32') # diff numbers 0..255
-        # samples_255[2*i+1,:,:,:] = real_flow 
-        images = np.insert(images, i*2+1, real_flow, axis=0)
+        real_uvflow = fixed_real_data[i,:]
+        real_uvflow = real_uvflow.reshape(IM_DIM,IM_DIM,2)
+        real_flowimg = fh.computeFlowImg(real_uvflow)  # (32, 32, 3) color img!
+        print(real_flowimg.shape)
+        real_flowimg = real_flowimg.reshape(IM_DIM,IM_DIM,3).astype('int32')
+        print(real_flowimg.shape)
+        real_flowimg_T = np.transpose(real_flowimg, [2,0,1])  #  (3, 32, 32)
+        print(real_flowimg_T.shape)  
+        images = np.insert(images, i*2+1, real_flowimg, axis=0)
+    print(images.shape)
+    # images.reshape((2*BATCH_SIZE, 3, IM_DIM, IM_DIM))
+    lib.save_images.save_images(images, 'cond_frames_batch.jpg')    		
 
-        # last_frame = images[i,:,:,:].astype('int32')  
-        #last_frame = last_frame.reshape(IM_DIM,IM_DIM,3)
-        #last_frame_transposed = last_frame.reshape(3,IM_DIM,IM_DIM) # (3072,)
-        # samples_255[2*i,:,:,:] = last_frame # last frame left of generated sample
-# samples_255= np.insert(samples_255, i*2, fixed_cond_data_int[i],axis=0)
 
-    lib.save_images.save_images(images.reshape((2*BATCH_SIZE, 3, IM_DIM, IM_DIM)), 'cond_frames_batch.jpg')    		
-
+# show flow 
+#flowimage_T = np.transpose(flowimg, [2,0,1])  #  (3, 200, 200)
+#save_images(flowimage_T.reshape((1,3,TARGET_SIZE,TARGET_SIZE)), outpath+"flowsintel10.jpg")
